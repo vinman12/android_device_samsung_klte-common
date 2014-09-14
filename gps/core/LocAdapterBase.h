62d959d2cb6c5a4c6f8987be7195b80aa12ed0c7
Matt Filetto <matt.filetto@gmail.com>
2014-09-14 02:17:48 -0700

    klte-common: sync GPS HAL with LNX.LA.3.5.2.2-03610-8x74.0
    minus one patch
    
    Change-Id: I238ede439414a414738a0e46a90fc0036535266c

--- gps/core/LocAdapterBase.h
+++ gps/core/LocAdapterBase.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -97,6 +97,7 @@ public:
     virtual bool requestNiNotify(GpsNiNotification &notify,
                                  const void* data);
     inline virtual bool isInSession() { return false; }
+    ContextBase* getContext() const { return mContext; }
 };
 
 } // namespace loc_core
