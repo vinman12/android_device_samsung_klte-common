3b7616e0254199d84474526836b9b9a55c04c3dd
Matt Filetto <matt.filetto@gmail.com>
2014-11-19 16:48:51 -0800

    Revert "klte-common: move wpa_supplicant.conf to device tree"
    This reverts commit 1d93fc1df52938f1c1539571c25873cc53875d2c.

--- klte.mk
+++ klte.mk
@@ -114,8 +114,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-   $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-   $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+   $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 # common msm8974
 $(call inherit-product, device/samsung/msm8974-common/msm8974.mk)
