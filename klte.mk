114d75fcde48b12ca54f2a8a9d1e74d4cc7089e7
Matt Filetto <matt.filetto@gmail.com>
2014-11-24 23:15:41 -0800

    klte-common: build more wifi parts
    Change-Id: If9e6c76c34a67e1e3246391f52ca9f33a444131e

--- klte.mk
+++ klte.mk
@@ -114,8 +114,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
     hostapd.accept \
+    hostapd_default.conf \
     hostapd.deny \
     hostapd \
+    libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
 
